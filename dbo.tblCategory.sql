94cca3517700c29f68eefc185f4fe343de98539b
Vu Dang Khoa <87051112+khoavukhoa3004@users.noreply.github.com>
2022-04-22 21:50:54 +0700

    Add files via upload

---
+++ dbo.tblCategory.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[tblCategory] (
+    [id]            INT           IDENTITY (1, 1) NOT NULL,
+    [category_name] NVARCHAR (50) NOT NULL,
+    PRIMARY KEY CLUSTERED ([id] ASC)
+);
+
---
+++ fAdmin.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient; //Sử dụng thư viện để làm việc SQL server
+
+namespace Quanlycf
+{
+    
+    public partial class fAdmin : Form
+    {
+        public fAdmin()
+        {
+            InitializeComponent();
+        }
+
+        private void fAdmin_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'masterDataSet1.tblUser' table. You can move, or remove it, as needed.
+            this.tblUserTableAdapter1.Fill(this.masterDataSet1.tblUser);
+            // TODO: This line of code loads data into the 'masterDataSet.tblUser' table. You can move, or remove it, as needed.
+            this.tblUserTableAdapter.Fill(this.masterDataSet.tblUser);
+            LoadDataGridView();
+        }
+
+        private void LoadDataGridView()
+        {
+            Class.Function.Connect();
+            string sql;
+            sql = "SELECT * FROM tblUser";
+            DataTable tblCL = Class.Function.GetDataToTable(sql); //Đọc dữ liệu từ bảng
+            dvgUser.DataSource = tblCL; //Nguồn dữ liệu            
+            //dvgUser.AutoResizeColumns(DataGridViewAutoSizeColumnsMode.Fill);
+
+
+            //dvgUser.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            //dvgUser.AllowUserToOrderColumns = true;
+            //dvgUser.AllowUserToResizeColumns = true;
+            // dataGridView1.AutoResizeColumns(DataGridViewAutoSizeColumnsMode.Fill);
+
+            dvgUser.AllowUserToAddRows = false; //Không cho người dùng thêm dữ liệu trực tiếp
+            dvgUser.EditMode = DataGridViewEditMode.EditProgrammatically;
+            Class.Function.Disconnect();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+        
+        }
+
+        private void fileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void toolsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void customizeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick_1(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void dvgUser_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            dvgUser.Show();
+        }
+
+        private void tabPage1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void addNV_Click(object sender, EventArgs e)
+        {
+            fAddNV temp = new fAddNV();
+            temp.Show();
+        }
+    }
+}
