60ae8f374d4ebb6448152abc1ce91bca679deddb
Tan Tran <74975483+tanminh0708@users.noreply.github.com>
2022-05-09 00:36:00 +0700

    Update OrderDAO.cs

--- Quanlycf/Staff/OrderDAO.cs
+++ Quanlycf/Staff/OrderDAO.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,24 +6,38 @@ using System.Threading.Tasks;
 using System.Data;
 namespace Quanlycf.Staff
 {
-
-    public class OrderDAO
+    class OrderDetailDAO
     {
-        private static OrderDAO instance;
-        public static OrderDAO Instance
+        private static OrderDetailDAO instance;
+
+        public static OrderDetailDAO Instance
         {
-            get { if (instance == null) instance = new OrderDAO(); return OrderDAO.instance; }
-            private set { OrderDAO.instance = value; }
-
+            get { if (instance == null) instance = new OrderDetailDAO(); return OrderDetailDAO.instance; }
+            private set { OrderDetailDAO.instance = value; }
         }
-        public static int OrderWidth = 70;
-        public static int OrderHeight = 20;
-        private OrderDAO()
-        {
-
-        }
-
+        private OrderDetailDAO() { }
        
+        public List<OrderDetail> GetListBillInfo(int id)
+        {
+            List<OrderDetail> listBillInfo = new List<OrderDetail>();
 
+            DataTable data = DataProvider.Instance.ExecuteQuery("SELECT * FROM dbo.tblOrderDetail WHERE @order_id = " + id);
+
+            foreach (DataRow item in data.Rows)
+            {
+                OrderDetail info = new OrderDetail(item);
+                listBillInfo.Add(info);
+            }
+
+            return listBillInfo;
+        }
+
+        public void InsertBillInfo(int idBill, int idFood, int count)
+        {
+            DataProvider.Instance.ExecuteNonQuery("USP_InsertBillInfo @order_id , @category_id , @num", new object[] { idBill, idFood, count });
+        }
     }
+
+
 }
+
