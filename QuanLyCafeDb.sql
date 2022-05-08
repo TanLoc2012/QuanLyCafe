b24791d5a8da6e7c19f597b0736c7c769d725020
Tan Tran <74975483+tanminh0708@users.noreply.github.com>
2022-05-09 00:22:44 +0700

    Update QuanLyCafeDb.sql

--- QuanLyCafeDb.sql
+++ QuanLyCafeDb.sql
@@ -53,3 +53,61 @@ VALUES('Staff')
 
 INSERT INTO tblRole(role_name)
 VALUES('Admin')
+
+SET IDENTITY_INSERT [dbo].[Food] ON
+INSERT [dbo].[Product] ([id], [title], [category_id], [price],[thumbnail],[descriptionP],[deleted],[discount]) VALUES (1, N'Mực một nắng nước sa tế', 1, 120000,1,1,1,1)
+INSERT [dbo].[Product] ([id], [title], [category_id], [price],[thumbnail],[descriptionP],[deleted],[discount]) VALUES (2, N'Nghêu hấp xả', 1, 50000,1,1,1,1)
+INSERT [dbo].[Product]  ([id], [title], [category_id], [price],[thumbnail],[descriptionP],[deleted],[discount]) VALUES (3, N'Dú dê nướng sữa', 2, 60000,1,1,1,1)
+INSERT [dbo].[Product]  ([id], [title], [category_id], [price],[thumbnail],[descriptionP],[deleted],[discount]) VALUES (4, N'Heo rừng nướng muối ớt', 3, 75000,1,1,1,1)
+INSERT [dbo].[Product]  ([id], [title], [category_id], [price],[thumbnail],[descriptionP],[deleted],[discount]) VALUES (5, N'Cơm chiên mushi', 4, 999999,1,1,1,1)
+INSERT [dbo].[Product] ([id], [title], [category_id], [price],[thumbnail],[descriptionP],[deleted],[discount]) VALUES (6, N'7Up', 5, 15000,1,1,1,1)
+INSERT [dbo].[Product]  ([id], [title], [category_id], [price],[thumbnail],[descriptionP],[deleted],[discount]) VALUES (7, N'Cafe', 5, 12000,1,1,1,1)
+SET IDENTITY_INSERT [dbo].[Food] OFF
+SELECT * FROM dbo.tblProduct
+CREATE PROC [dbo].[USP_InsertBill]
+AS
+BEGIN
+	INSERT dbo.tblOrders 
+	        (  
+	          ngay_ban,
+	          total_money,
+	          statusOrder
+	        )
+	VALUES  (  GETDATE() , -- DateCheckIn - date
+	          0, -- TongTien - int
+	          0  -- status - int
+	        )
+END
+GO
+CREATE PROC [dbo].[USP_InsertBillInfo]
+@order_id INT, @id INT, @num INT
+AS
+BEGIN
+
+	DECLARE @isExitsBillInfo INT
+	DECLARE @foodCount INT = 1
+	
+	SELECT @isExitsBillInfo = id, @foodCount = b.num 
+	FROM dbo.tblOrderDetail AS b 
+	WHERE order_id = @order_id AND id = @id
+
+	IF (@isExitsBillInfo > 0)
+	BEGIN
+		DECLARE @newCount INT = @foodCount + @num
+		IF (@newCount > 0)
+			UPDATE dbo.tblOrderDetail	SET num = @foodCount + @num WHERE id = @id
+		ELSE
+			DELETE dbo.tblOrderDetail WHERE order_id  = @order_id  AND id = @id
+	END
+	ELSE
+	BEGIN
+		INSERT	dbo.tblOrderDetail
+        ( order_id, id, num )
+		VALUES  ( @order_id, -- idBill - int
+          @id, -- idFood - int
+          @num  -- count - int
+          )
+	END
+END
+GO
+
