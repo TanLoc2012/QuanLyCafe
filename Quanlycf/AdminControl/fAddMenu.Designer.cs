d86558cadc34024282416b224632e699bbe94e84
dangkhanh911 <79183279+dangkhanh911@users.noreply.github.com>
2022-05-10 16:57:25 +0700

    add menu

---
+++ Quanlycf/AdminControl/fAddMenu.Designer.cs
@@ -0,0 +1,270 @@
+﻿namespace Quanlycf
+{
+    partial class fAddMenu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.thumbnailBox = new System.Windows.Forms.TextBox();
+            this.createBox = new System.Windows.Forms.Button();
+            this.categoryBox = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.titleBox = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.priceBox = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.discountBox = new System.Windows.Forms.TextBox();
+            this.descriptionBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.managerCoffeeDataSet3 = new Quanlycf.ManagerCoffeeDataSet3();
+            this.tblProductBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.tblProductTableAdapter = new Quanlycf.ManagerCoffeeDataSet3TableAdapters.tblProductTableAdapter();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.managerCoffeeDataSet3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tblProductBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.thumbnailBox);
+            this.panel1.Controls.Add(this.createBox);
+            this.panel1.Controls.Add(this.categoryBox);
+            this.panel1.Controls.Add(this.label7);
+            this.panel1.Controls.Add(this.titleBox);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.priceBox);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.discountBox);
+            this.panel1.Controls.Add(this.descriptionBox);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Location = new System.Drawing.Point(1, 2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1119, 478);
+            this.panel1.TabIndex = 2;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(137, 257);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 27);
+            this.label1.TabIndex = 18;
+            this.label1.Text = "Mô tả:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // thumbnailBox
+            // 
+            this.thumbnailBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.thumbnailBox.Location = new System.Drawing.Point(243, 173);
+            this.thumbnailBox.Name = "thumbnailBox";
+            this.thumbnailBox.Size = new System.Drawing.Size(481, 41);
+            this.thumbnailBox.TabIndex = 17;
+            this.thumbnailBox.TextChanged += new System.EventHandler(this.thumbnailBox_TextChanged);
+            // 
+            // createBox
+            // 
+            this.createBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.createBox.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.createBox.Location = new System.Drawing.Point(800, 93);
+            this.createBox.Name = "createBox";
+            this.createBox.Size = new System.Drawing.Size(126, 107);
+            this.createBox.TabIndex = 16;
+            this.createBox.Text = "Tạo mới";
+            this.createBox.UseVisualStyleBackColor = false;
+            this.createBox.Click += new System.EventHandler(this.createBox_Click);
+            // 
+            // categoryBox
+            // 
+            this.categoryBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.categoryBox.Location = new System.Drawing.Point(243, 97);
+            this.categoryBox.Name = "categoryBox";
+            this.categoryBox.Size = new System.Drawing.Size(481, 41);
+            this.categoryBox.TabIndex = 14;
+            this.categoryBox.TextChanged += new System.EventHandler(this.categoryBox_TextChanged);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Arial", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(84, 107);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(132, 27);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Danh mục:";
+            this.label7.Click += new System.EventHandler(this.label7_Click);
+            // 
+            // titleBox
+            // 
+            this.titleBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titleBox.Location = new System.Drawing.Point(243, 21);
+            this.titleBox.Name = "titleBox";
+            this.titleBox.Size = new System.Drawing.Size(481, 41);
+            this.titleBox.TabIndex = 12;
+            this.titleBox.TextChanged += new System.EventHandler(this.titleBox_TextChanged);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Arial", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(154, 27);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(62, 27);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Tên:";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // priceBox
+            // 
+            this.priceBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.priceBox.Location = new System.Drawing.Point(243, 320);
+            this.priceBox.Name = "priceBox";
+            this.priceBox.Size = new System.Drawing.Size(481, 41);
+            this.priceBox.TabIndex = 10;
+            this.priceBox.TextChanged += new System.EventHandler(this.priceBox_TextChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Arial", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(159, 320);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(57, 27);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Giá:";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(77, 183);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(139, 27);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Thumbnail:";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // discountBox
+            // 
+            this.discountBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.discountBox.Location = new System.Drawing.Point(243, 390);
+            this.discountBox.Name = "discountBox";
+            this.discountBox.Size = new System.Drawing.Size(481, 41);
+            this.discountBox.TabIndex = 6;
+            this.discountBox.TextChanged += new System.EventHandler(this.discountBox_TextChanged);
+            // 
+            // descriptionBox
+            // 
+            this.descriptionBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.descriptionBox.Location = new System.Drawing.Point(243, 249);
+            this.descriptionBox.Name = "descriptionBox";
+            this.descriptionBox.Size = new System.Drawing.Size(481, 41);
+            this.descriptionBox.TabIndex = 6;
+            this.descriptionBox.TextChanged += new System.EventHandler(this.descriptionBox_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(100, 399);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(116, 27);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Giảm giá:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(85, 330);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(130, 27);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Mô tả:";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // managerCoffeeDataSet3
+            // 
+            this.managerCoffeeDataSet3.DataSetName = "DataSet1";
+            this.managerCoffeeDataSet3.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // tblProductBindingSource
+            // 
+            this.tblProductBindingSource.CurrentChanged += new System.EventHandler(this.tblProductBindingSource_CurrentChanged);
+            // 
+            // tblProductTableAdapter
+            // 
+            this.tblProductTableAdapter.ClearBeforeFill = true;
+            // 
+            // fAddMenu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1122, 464);
+            this.Controls.Add(this.panel1);
+            this.Name = "fAddMenu";
+            this.Text = "Biểu mẫu thêm món ăn";
+            this.Load += new System.EventHandler(this.fAddMenu_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.managerCoffeeDataSet3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tblProductBindingSource)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button createBox;
+        private System.Windows.Forms.TextBox categoryBox;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox titleBox;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox priceBox;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox descriptionBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox discountBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.BindingSource tblProductBindingSource;
+        private System.Windows.Forms.TextBox thumbnailBox;
+        private System.Windows.Forms.Label label1;
+        private ManagerCoffeeDataSet3 managerCoffeeDataSet3;
+        private ManagerCoffeeDataSet3TableAdapters.tblProductTableAdapter tblProductTableAdapter;
+    }
+}
---
+++ Quanlycf/AdminControl/fAddMenu.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Quanlycf
+{
+    public partial class fAddMenu : Form
+    {
+        public fAddMenu()
+        {
+            InitializeComponent();
+        }
+
+        private void createBox_Click(object sender, EventArgs e)
+        {
+
+            if (categoryBox.Text == "" || titleBox.Text == "" || int.Parse(priceBox.Text) <= 0 || thumbnailBox.Text == "" || descriptionBox.Text == "" || (int.Parse(discountBox.Text) <0 || int.Parse(discountBox.Text) >= 100))
+            {
+                MessageBox.Show("Bạn chưa điền đầy đủ thông tin. Vui lòng kiểm tra lại!", "Missing information", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            string s = "INSERT INTO [dbo].[tblProduct] VALUES (" + categoryBox.Text + ",'" + titleBox.Text + "','" + priceBox.Text + "','" + thumbnailBox.Text + "','" + descriptionBox.Text + "','" + '0' + discountBox.Text + "');";
+
+            Class.Function.Connect();
+            if (Class.Function.updateData(s))
+            {
+                Class.Function.Disconnect();
+                this.Hide();
+            }
+            //Class.Function.Disconnect();
+        }
+
+        private void fAddMenu_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'managerCoffeeDataSet3.tblProduct' table. You can move, or remove it, as needed.
+            this.tblProductTableAdapter.Fill(this.managerCoffeeDataSet3.tblProduct);
+        }
+
+        private void tblProductBindingSource_CurrentChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void thumbnailBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void categoryBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void titleBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void priceBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void discountBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void descriptionBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
